64cd62ef78741e93d2e72eb7d1404add65570223
Dante291 <gparag222@gmail.com>
2023-07-13 23:40:59 +0530

    3rd

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,22 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:meal_app/category_meals_screen.dart';
 
 class Category_item extends StatelessWidget {
   final String title;
   final Color color;
 
-  Category_item(this.color, this.title);
+  // ignore: use_key_in_widget_constructors
+  const Category_item(this.color, this.title);
+
+  void screenCategory(BuildContext ctx) {
+    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
+      return CategoryMealScreen();
+    }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(title),
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            color.withOpacity(0.7),
-            color,
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          borderRadius: BorderRadius.circular(15)),
+    return InkWell(
+      onTap: () => screenCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              color.withOpacity(0.7),
+              color,
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+            borderRadius: BorderRadius.circular(15)),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
     );
   }
 }
