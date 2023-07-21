09041e4d249fc625329ee04cb38d2d8e80797a5f
Dante291 <gparag222@gmail.com>
2023-07-21 22:11:45 +0530

    recipe details

--- lib/meal_detail_screen.dart
+++ lib/meal_detail_screen.dart
@@ -14,58 +14,71 @@ class MealDetailScreen extends StatelessWidget {
     final mealTitle = routeArgs['title'] as String;
     final selectedimage =
         DUMMY_MEALS.firstWhere((element) => element.id == mealId);
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            margin: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
-            decoration: BoxDecoration(
-              color: Theme.of(context).primaryColor,
-              borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10)),
-            ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10)),
-              child: Image.network(
-                selectedimage.imageUrl,
-                fit: BoxFit.cover,
-                height: 300,
-                width: double.infinity,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: bacolor,
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Color.fromARGB(255, 205, 204, 204),
-                    spreadRadius: 3,
-                    blurRadius: 5,
-                  )
-                ],
-              ),
-              child: Text(
-                mealTitle,
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-          )
-        ],
+    return Material(
+      type: MaterialType.transparency,
+      child: Container(
+        height: double.infinity,
+        width: double.infinity,
+        color: bacolor,
+        child: Stack(
+          children: [
+            Positioned(
+                top: 60,
+                left: 7,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    selectedimage.imageUrl,
+                  ),
+                  radius: 200,
+                )),
+          ],
+        ),
       ),
     );
   }
 }
+//           Container(
+//             color: Colors.white,
+//             child: Column(children: [
+//               ClipRRect(
+//                 borderRadius: const BorderRadius.only(
+//                     bottomLeft: Radius.circular(30),
+//                     bottomRight: Radius.circular(30)),
+//                 child: Image.network(
+//                   selectedimage.imageUrl,
+//                   fit: BoxFit.cover,
+//                   height: 320,
+//                   width: double.infinity,
+//                 ),
+//               ),
+//               const SizedBox(
+//                 height: 8,
+//               ),
+//               Padding(
+//                 padding: const EdgeInsets.all(10),
+//                 child: Container(
+//                   padding: const EdgeInsets.all(15),
+//                   decoration: BoxDecoration(
+//                     color: bacolor,
+//                     borderRadius: BorderRadius.circular(15),
+//                     boxShadow: const [
+//                       BoxShadow(
+//                         color: Color.fromARGB(255, 205, 204, 204),
+//                         spreadRadius: 3,
+//                         blurRadius: 5,
+//                       )
+//                     ],
+//                   ),
+//                   child: Text(mealTitle,
+//                       style: const TextStyle(
+//                           color: Color.fromARGB(255, 255, 255, 255),
+//                           fontSize: 24,
+//                           fontWeight: FontWeight.w600,
+//                           fontFamily: 'RobotoCondensed')),
+//                 ),
+//               ),
+//             ])),
+//      ] ),
+//     );
+//   }
+// }
